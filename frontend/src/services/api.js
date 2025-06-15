89595703d0d8da9c78fd6f979881495a1a2c8f83
Deneth  Godakandagei <138301396+DenethGodakandagei@users.noreply.github.com>
2025-06-15 23:22:20 +0530

    server aded

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api/',
+  baseURL: 'https://hotel-management-system-1-wral.onrender.com/api',
 });
 
 export default api;
